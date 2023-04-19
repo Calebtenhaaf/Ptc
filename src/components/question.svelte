f786ab41ee8d401766d8583f8e73dd3a5e0dd078
Caleb tenHaaf <calebtenHaaf08@gmail.com>
2023-04-19 16:04:19 +0000

    Atempted quiz work (fail)

--- src/components/question.svelte
+++ src/components/question.svelte
@@ -42,3 +42,29 @@
 </p>
 
 {/if}
+
+<script>
+    export let answer = true;
+</script>
+
+{#if answer}
+    <p class ="correct">Good Job!</p>
+
+ {:else}
+    <p class ="incorrect">Not quite...</p>
+{/if}
+
+    <style>
+        .correct
+        .incorrect {
+            font-weight: bold;
+            text-decoration: underline;
+        }
+        .correct {
+            color: green; 
+        }
+
+        .incorrect {
+            color: red;
+        }
+        </style>
